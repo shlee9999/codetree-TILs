e11ee38b52ae16d4d382700d6e8078002508add8
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-11-10 13:50:39 +0900

    Title: 줄 겹치게 접기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231110/줄 겹치게 접기/rope-folding.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+n, l =  map(int, input().split())
+li = [int(input()) for _ in range(n)]
+arr1 = []
+arr2 = []
+result = []
+for fold_point in range(1, l):
+    for i in range(n):
+        if li[i] == fold_point:
+            continue
+        if li[i] < fold_point:
+            diff = fold_point - li[i]
+            arr2.append(fold_point+diff)
+        else:
+            arr1.append(li[i])
+    if len(arr2) > len(arr1):
+        arr1, arr2 = arr2, arr1
+    for x in arr2:
+        if x not in arr1:
+            break
+    else:
+        result.append(fold_point)
+print(len(result))
