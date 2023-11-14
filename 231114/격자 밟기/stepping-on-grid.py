7181859b35edb7a090c069fe327103f7b23eed3c
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-11-14 15:12:27 +0900

    Title: 격자 밟기, Time: 329ms, Memory: 29MB, Status: Passed - CodeTree

--- 231114/격자 밟기/stepping-on-grid.py
+++ 231114/격자 밟기/stepping-on-grid.py
@@ -9,8 +9,9 @@ graph = [[1] * 5 for _ in range(5)]
 for _ in range(k):
     r, c = map(int, input().split())
     graph[r - 1][c - 1] = 0  # 0은 벽, 1은 길
-    visited[r-1][c-1]= True
-
+    visited[r - 1][c - 1] = True
+visited[0][0] = True
+visited[4][4] = True
 result = 0
 
 
@@ -22,15 +23,13 @@ def check(x, y):
 
 def dfs(x_a, y_a, x_b, y_b):
     global result
-    visited[x_a][y_a] = True
-    visited[x_b][y_b] = True
-    for x in visited:
-        if False in x:
-            break
-    else:
-        if x_a == x_b and y_a == y_b:
+    if x_a == x_b and y_a == y_b:
+        for x in visited:
+            if False in x:
+                return
+        else:  # 모두 방문하고 딱 만남
             result += 1
-        return
+            return
     for i in range(4):
         nx_a = x_a + dx[i]
         ny_a = y_a + dy[i]
@@ -38,6 +37,8 @@ def dfs(x_a, y_a, x_b, y_b):
             nx_b = x_b + dx[j]
             ny_b = y_b + dy[j]
             if check(nx_a, ny_a) and check(nx_b, ny_b):  # 둘 다 이동 가능
+                visited[nx_a][ny_a] = True
+                visited[nx_b][ny_b] = True
                 dfs(nx_a, ny_a, nx_b, ny_b)
                 visited[nx_a][ny_a] = False
                 visited[nx_b][ny_b] = False
