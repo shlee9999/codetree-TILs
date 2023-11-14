76fcb63249885798f3d78b0382f0ce2027bb8d54
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-11-14 15:03:15 +0900

    Title: 격자 밟기, Time: 743ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 231114/격자 밟기/stepping-on-grid.py
@@ -0,0 +1,48 @@
+import sys
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+input = sys.stdin.readline
+visited = [[False] * 5 for _ in range(5)]
+k = int(input())
+graph = [[1] * 5 for _ in range(5)]
+for _ in range(k):
+    r, c = map(int, input().split())
+    graph[r - 1][c - 1] = 0  # 0은 벽, 1은 길
+    visited[r-1][c-1]= True
+
+result = 0
+
+
+def check(x, y):
+    if 0 <= x < 5 and 0 <= y < 5 and graph[x][y] and not visited[x][y]:
+        return True
+    return False
+
+
+def dfs(x_a, y_a, x_b, y_b):
+    global result
+    visited[x_a][y_a] = True
+    visited[x_b][y_b] = True
+    for x in visited:
+        if False in x:
+            break
+    else:
+        if x_a == x_b and y_a == y_b:
+            result += 1
+        return
+    for i in range(4):
+        nx_a = x_a + dx[i]
+        ny_a = y_a + dy[i]
+        for j in range(4):
+            nx_b = x_b + dx[j]
+            ny_b = y_b + dy[j]
+            if check(nx_a, ny_a) and check(nx_b, ny_b):  # 둘 다 이동 가능
+                dfs(nx_a, ny_a, nx_b, ny_b)
+                visited[nx_a][ny_a] = False
+                visited[nx_b][ny_b] = False
+
+
+dfs(0, 0, 4, 4)
+
+print(result)
